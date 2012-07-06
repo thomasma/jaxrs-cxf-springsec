ce1ffe3d2e2e4099533eb0ffc614a906288a449c
Mathew Thomas <mattazoid@gmail.com>
2012-07-05 22:00:27 -0400

    initial load

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-jaxrs-cxf-springsec
-===================
+Secure RESTful Services with Maven, Spring, Apache CXF and Spring Security
+
+See http://blogs.justenougharchitecture.com/?p=499 for detials.
---
+++ src/test/java/RestTest.java
@@ -0,0 +1,21 @@
+
+public class RestTest {
+	public static void main(String[] args) {
+//		Client c = Client.create();
+//
+//		// plain text
+//		WebResource r = c
+//				.resource("http://localhost:9090/jaxrs/services/timeoftheday/asplaintext/mathew");
+//		System.out.println("Plain Text=>> " + r.get(String.class));
+//
+//		// json
+//		r = c.resource("http://localhost:9090/jaxrs/services/timeoftheday/asjson/mathew");
+//		c.addFilter(new HTTPBasicAuthFilter("johndoe", "password"));
+//		System.out.println("JSON=>> " + r.get(String.class));
+//
+//		// xml
+//		r = c.resource("http://localhost:9090/jaxrs/services/timeoftheday/asxml/mathew");
+//		c.addFilter(new HTTPBasicAuthFilter("admin", "password"));
+//		System.out.println("XML=>> " + r.get(String.class));
+	}
+}
